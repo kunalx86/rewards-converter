7f1e54ccb1e4f26f7abd92d75c051c1b2a080157
Kunal Joshi <mail.kunaljoshi@gmail.com>
2024-06-06 12:49:20 +0530

    Implemented RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,24 @@
+public class RewardValue {
+    private double cash;
+    private static double factor = 0.0035;
+
+    public RewardValue(double cash) {
+        this.cash = cash;
+    }
+
+    public RewardValue(int miles) {
+        this.cash = _getCashValue(miles);
+    }
+
+    public int getMilesValue() {
+        return (int) (this.cash / factor);
+    }
+
+    public double getCashValue() {
+        return this.cash;
+    }
+
+    private double _getCashValue(int miles) {
+        return miles * factor;
+    }
+}
